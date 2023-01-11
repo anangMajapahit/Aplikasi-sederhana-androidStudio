8cbf51c562d901bfa2859a63b3d4f4396c25e100
AnangMajapahit <86045046+anangMajapahit@users.noreply.github.com>
2023-01-11 19:24:10 +0700

    main

---
+++ iHiking/app/src/main/java/com/example/ihiking/i_hiking/splash_screen.java
@@ -0,0 +1,30 @@
+package com.example.ihiking.i_hiking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        Splash();
+    }
+    public void Splash(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },3000);
+    }
+}
